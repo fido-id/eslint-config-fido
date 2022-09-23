0c0a4fbc72ba5505e04f13774d8a0ea6bc2fb7ba
Enrico <enrico@fido.id>
2022-09-23 17:12:29 +0200

    feat(eslint): added new rules

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ module.exports = {
     '@typescript-eslint/consistent-type-assertions': 'warn',
     'no-promise-executor-return': 'warn',
     'max-params': 'warn',
+    'react/no-namespace': 'off',
+    'react/iframe-missing-sandbox': 'off',
+    'react/jsx-no-useless-fragment': 'warn',
+    'react/no-unescaped-entities': 'warn',
+    '@typescript-eslint/no-unused-expressions': 'warn',
   },
   settings: {
     react: {
