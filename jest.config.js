3ff37b953fa90a29ec1a81336193b225870d5b9e
Marco Cotrufo <marco.cotrufo@devncode.it>
2025-08-08 15:05:24 +0200

    ci(github): collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 /** @type {import('jest').Config} */
 module.exports = {
-    testEnvironment: "node",
-    testMatch: ["**/tests/**/*.test.js"],
-    verbose: true,
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.js"],
+  verbose: true,
+  collectCoverage: true,
+  coverageReporters: ["text", "cobertura"],
 };
